9e5107a0f67f704f16683459eafce678604f5847
CaldwellDerek <djohncaldwell@gmail.com>
2022-06-21 15:08:47 -0700

    Database, Repository, TermDAO and Term Class created

--- app/src/main/java/com/caldwell/c196studentscheduler/UI/MainActivity.java
+++ app/src/main/java/com/caldwell/c196studentscheduler/UI/MainActivity.java
@@ -1,8 +1,12 @@
-package com.caldwell.c196studentscheduler;
+package com.caldwell.c196studentscheduler.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.caldwell.c196studentscheduler.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void viewSchedule(View view) {
+        Intent i = new Intent(MainActivity.this, TermList.class);
+        startActivity(i);
     }
 }
